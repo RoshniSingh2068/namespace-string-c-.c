56ec0b64f2a48584dc86f626d590c36d4244af50
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:14:55 +0530

    Create namespace string c++.c

---
+++ namespace string c++.c
@@ -0,0 +1,13 @@
+// string namespace
+// you might see some c++ programs that run without using namespace std
+// omitting and replacing std by
+
+#include <iostream>
+#include <string>
+
+int main(){
+    std :: string greeting = "hello world ";
+    std :: cout << greeting ;
+    return 0;
+
+}
